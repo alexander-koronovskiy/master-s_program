b628541e51612604d567c054435de18f1899039b
alexander <alexander.koronovskiy@gmail.com>
2019-03-09 00:39:37 +0400

    binary steps

--- MyTest.py
+++ MyTest.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot
 from scipy.interpolate import interp1d
 import numpy as np
 
-ITER = 1000
+ITER = 1024
 
 
 # функция
@@ -50,17 +50,16 @@ def do_profile(
 
 # входные данные: массив / выходные - тоже массив данных, но с разным n
 def do_dfa(array, steps):
+    for i in range(2,10):
+        x = np.linspace(0, ITER, num=steps**i, endpoint=True)
+        y = array[::ITER // (steps**i)]
+        print(x,y)
+        f = interp1d(x, y)
+        x_new = np.linspace(0, ITER, num=ITER, endpoint=True)
 
-    print(ITER // steps)
-    x = np.linspace(0, 1000, num=steps, endpoint=True)
-    y = array[::ITER // steps]
-    f = interp1d(x, y)
-    x_new = np.linspace(0, 1000, num=1000, endpoint=True)
-
-    matplotlib.pyplot.plot(f(x_new))
-    matplotlib.pyplot.plot(array)
-    matplotlib.pyplot.show()
+        matplotlib.pyplot.plot(f(x_new))
+        matplotlib.pyplot.show()
 
 
 if __name__ == '__main__':
-    data = do_dfa(do_profile(), 10)
+    data = do_dfa(do_profile(), 2)
